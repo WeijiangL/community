cff1dd927319b68ced26bfeb7744b3ad584e4c66
ldx <1256095794@qq.com>
2019-07-27 15:16:15 +0800

    添加了导航条

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ##资料
 https://spring.io/guides/gs/serving-web-content/
-#bootstrap
+##bootstrap
 https://v3.bootcss.com/components/
+##github API
+https://developer.github.com/apps/
+
 
--- src/main/java/com/weijiang/controller/Hello.java
+++ src/main/java/com/weijiang/controller/Hello.java
@@ -1,16 +1,13 @@
 package com.weijiang.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hello {
 
-    @GetMapping("/ABC")
-    public String Hellow(@RequestParam("name") String name , Model model){
-        model.addAttribute("ename" , name);
+    @GetMapping("/")
+    public String Hellow(){
         return "hello";
     }
 }
